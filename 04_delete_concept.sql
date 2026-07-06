f2990eb73ef6dcb1ee7f42ac36bd47867b6c499a
Uday Aggarwal <aggarwal.uday26@gmail.com>
2026-07-06 15:48:23 +0530

    added delete concepts

---
+++ 04_delete_concept.sql
@@ -0,0 +1,54 @@
+SELECT * FROM Customers;
+
+--Insert a test row in the Customer table
+INSERT INTO Customers (FirstName, LastName, Email, Phone, City, State, Country) 
+VALUES
+('Test', 'User', 'testuser@gmail.com', '9999999999', 'Delhi', 'Delhi', 'India');
+
+--Proper deletion workflow
+-- STEP 1 (Select the row to be deleted)
+SELECT * FROM Customers
+WHERE FirstName = 'Test';
+
+-- STEP 2 (Delete the record)
+DELETE FROM Customers
+WHERE FirstName = 'Test';
+
+-- STEP 3 (Verify)
+SELECT * FROM Customers
+WHERE FirstName = 'Test';
+
+
+
+SELECT * FROM Categories;
+
+--Insert a test category
+INSERT INTO Categories (CategoryName, Description) 
+VALUES
+('Temporary Category', 'A category just for testing purposes');
+
+-- STEP 1
+SELECT * FROM Categories
+WHERE CategoryName = 'Temporary Category';
+
+-- STEP 2
+DELETE FROM Categories
+WHERE CategoryName = 'Temporary Category';
+
+-- STEP 3
+SELECT * FROM Categories
+WHERE CategoryName = 'Temporary Category';
+
+
+-- FK based Delete
+SELECT * FROM Customers;
+SELECT * FROM Orders;
+
+DELETE FROM Customers
+WHERE CustomerID = 1;
+/*
+Error because of FK Constraint
+The DELETE statement conflicted with the REFERENCE constraint "FK_Orders_Customers". 
+The conflict occurred in database "ECommerceDB", table "dbo.Orders", column 'CustomerID'.
+*/
+
